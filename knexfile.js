55223417319a1b3ca0d80bc4cfd9255b47790500
Matheus <mrmatheustor@gmail.com>
2020-06-18 18:47:34 -0300

    .

--- knexfile.js
+++ knexfile.js
@@ -32,9 +32,9 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: 'd990jcgvd0kl1d',
+      user:     'fptyftxizmaemc',
+      password: '8da6fc290a632882f3aa415da0cd212d608b2b72d34512cefda2e1e01f1baf03'
     },
     pool: {
       min: 2,
