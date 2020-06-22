0614d2755265fa0c755e48beac15cc95f9a87663
Matheus <mrmatheustor@gmail.com>
2020-06-22 18:13:25 -0300

    .

--- src/controllers/ListaChurrascoController.js
+++ src/controllers/ListaChurrascoController.js
@@ -7,11 +7,13 @@ module.exports = {
     const {churras_id} = request.params;
 
     const listaChurrasco = await connection('listaChurrasco')
+    .join('unidades', 'unidades.id', '=', 'listaChurraso.unidade_id')
     .join('itens', 'itens.id', '=', 'listaChurrasco.item_id')
     .join('churras', 'churras.id', '=', 'listaChurrasco.churras_id')
     .where('churras_id', churras_id)
     .select(['listaChurrasco.quantidade',
-    'itens.nomeItem', 
+    'itens.nomeItem',
+    'unidades.unidade', 
     'churras.nomeChurras']).catch(function(err) {
     console.error(err);
     });
