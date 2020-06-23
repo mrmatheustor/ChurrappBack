883692ec39dced7f8521d0a630f41c74a52250a3
Matheus <mrmatheustor@gmail.com>
2020-06-22 21:15:18 -0300

    .

--- src/controllers/ChurrasController.js
+++ src/controllers/ChurrasController.js
@@ -6,7 +6,7 @@ module.exports = {
     const { page = 1 } = request.query;
 
     const [count] = await connection('churras')
-    .count();
+    .count('id');
 
     const churras = await connection('churras')
     .join('usuarios', 'usuarios.id', '=', 'churras.usuario_id')
