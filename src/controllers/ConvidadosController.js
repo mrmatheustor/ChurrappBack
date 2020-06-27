6215d2ad630138860c7d612ae727f56162c38517
Matheus <mrmatheustor@gmail.com>
2020-06-27 16:59:59 -0300

    .

--- src/controllers/ConvidadosController.js
+++ src/controllers/ConvidadosController.js
@@ -29,7 +29,7 @@ module.exports = {
     const {valorPagar, churras_id} = request.body;
     const {usuario_id} = request.params;
 
-    const [condicao] = await connection('convidados').select('churras_id');
+    // const [condicao] = await connection('convidados').select('churras_id');
 
     // for(i = 0; i < condicao.length; i++) {
     //   if(condicao.churras_id !== churras_id) {
@@ -38,7 +38,7 @@ module.exports = {
     // }
     
 
-    const [id] = await connection('convidados').insert({
+    const id = await connection('convidados').insert({
         valorPagar,
         churras_id,
         usuario_id
@@ -47,7 +47,7 @@ module.exports = {
     });
     
 
-    return response.json({id});
+    return response.json(id);
 
   },
 };
