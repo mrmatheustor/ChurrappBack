aa5f691f86d4090e865b05a815b9a118e6c96b5d
Rafael França <rafaelfldasilva@gmail.com>
2020-08-01 18:33:43 -0300

    correção em tipos, adicionado subTipo para facilitar filtro no front

--- src/database/migrations/20200621011603_create_tipos.js
+++ src/database/migrations/20200621011603_create_tipos.js
@@ -3,6 +3,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('tipos', function(table){
         table.increments();
         table.string('tipo').notNullable();
+        table.integer('subTipo').notNullable();
     });
   
 };
