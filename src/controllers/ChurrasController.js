af0a28c29d0563086d71cecc812338d57ff8f573
Rafael França <rafaelfldasilva@gmail.com>
2020-07-31 22:49:44 -0300

    pegar foto usuario churrasco

--- src/controllers/ChurrasController.js
+++ src/controllers/ChurrasController.js
@@ -40,8 +40,8 @@ module.exports = {
     const churras = await connection('churras')
       .join('usuarios', 'usuarios.id', '=', 'churras.usuario_id')
       .join('fotos', function (){
-        this.on('fotos.id','=','usuarios.foto_id').as('fotoUsuario')
-        this.andOn('fotos.id','=','churras.foto_id').as('fotoChurras')
+        this.on('fotos.id','=','usuarios.foto_id')
+        this.andOn('fotos.id','=','churras.foto_id')
       })
       .limit(15)
       .orderBy('data')
