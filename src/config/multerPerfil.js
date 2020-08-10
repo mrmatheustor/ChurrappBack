23f61ea223ed7b140ca26cba5c6657140f99232a
Rafael França <rafaelfldasilva@gmail.com>
2020-08-10 12:12:22 -0300

    multerPerfil update para expo

--- src/config/multerPerfil.js
+++ src/config/multerPerfil.js
@@ -1,14 +1,11 @@
-const path = require('path');
 const crypto = require('crypto')
 const aws = require('aws-sdk')
 const multerS3 = require('multer-s3')
 
 module.exports = {
-    //dest: path.resolve(__dirname,'..','..','tmp','uploads'),
     storage: multerS3({
         s3: new aws.S3(),
         bucket:'churrappuploadteste/perfil',
-        //contentType:multerS3.AUTO_CONTENT_TYPE,
         acl:'public-read',
         metadata(req,file,cb){
             cb(null, {fieldName: file.fieldname});
@@ -18,28 +15,14 @@ module.exports = {
                 if(error) {
                     callBack(error);
                 }
-                const fileName = `${hash.toString('hex')}`
+                const fileName = `${hash.toString('hex')}.png`
     
                 callBack(null, fileName)
             })
         }
     }),
-    // limits:{
-    //     //aqui estamos pegando imagens de ate 5mb
-    //     fileSize: 5*1024*1024,
-    // },
-    // fileFilter:(require,file,callBack)=>{
-    //     const allowedMimes =[
-    //         'image/jpeg',
-    //         'image/pjpeg',
-    //         'image/png',
-    //         'image/gif',
-    //     ]
-
-    //     if(allowedMimes.includes(file.mimetype)){
-    //         callBack(null,true)
-    //     }else{
-    //         callBack(new Error("Invalid file type."))
-    //     }
-    // }
+    limits:{
+        //aqui estamos pegando imagens de ate 5mb
+        fileSize: 5*1024*1024,
+    },
 }
