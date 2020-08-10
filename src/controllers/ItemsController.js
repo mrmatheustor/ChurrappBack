51859d1bd78a3e60ee874f1521368457dc33e9f7
Matheus <mrmatheustor@gmail.com>
2020-08-10 16:15:15 -0300

    .

--- src/controllers/ItemsController.js
+++ src/controllers/ItemsController.js
@@ -46,7 +46,7 @@ module.exports = {
   async create(request, response) {
     const { nomeItem, descricao, tipo_id, unidade_id, precoMedio, foto_id} = request.body;
 
-    const [id] = await connection('itens').insert({
+    const id = await connection('itens').insert({
         nomeItem,
         descricao,
         tipo_id,
@@ -56,7 +56,7 @@ module.exports = {
     }).catch(function(err) {
       console.error(err);
     });
-    response.json({ id });
+    response.json( id );
   },
 
   async delete(request, response) {
