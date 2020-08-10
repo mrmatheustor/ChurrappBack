25499ae659b58d0c277ff379323aee4170a13b09
Rafael França <rafaelfldasilva@gmail.com>
2020-08-10 11:50:56 -0300

    multerPerfil update para expo

--- src/config/multerPerfil.js
+++ src/config/multerPerfil.js
@@ -8,7 +8,7 @@ module.exports = {
     storage: multerS3({
         s3: new aws.S3(),
         bucket:'churrappuploadteste/perfil',
-        //contentType:multerS3.AUTO_CONTENT_TYPE,
+        contentType:multerS3.AUTO_CONTENT_TYPE,
         acl:'public-read',
         metadata(req,file,cb){
             cb(null, {fieldName: file.fieldname});
@@ -24,22 +24,22 @@ module.exports = {
             })
         }
     }),
-    limits:{
-        //aqui estamos pegando imagens de ate 5mb
-        fileSize: 5*1024*1024,
-    },
-    fileFilter:(require,file,callBack)=>{
-        const allowedMimes =[
-            'image/jpeg',
-            'image/pjpeg',
-            'image/png',
-            'image/gif',
-        ]
+    // limits:{
+    //     //aqui estamos pegando imagens de ate 5mb
+    //     fileSize: 5*1024*1024,
+    // },
+    // fileFilter:(require,file,callBack)=>{
+    //     const allowedMimes =[
+    //         'image/jpeg',
+    //         'image/pjpeg',
+    //         'image/png',
+    //         'image/gif',
+    //     ]
 
-        if(allowedMimes.includes(file.mimetype)){
-            callBack(null,true)
-        }else{
-            callBack(new Error("Invalid file type."))
-        }
-    }
+    //     if(allowedMimes.includes(file.mimetype)){
+    //         callBack(null,true)
+    //     }else{
+    //         callBack(new Error("Invalid file type."))
+    //     }
+    // }
 }
