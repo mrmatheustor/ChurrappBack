24632ef5a4d6a8673d44674625ab88efc6e41ef8
Matheus <mrmatheustor@gmail.com>
2020-08-12 13:54:33 -0300

    .

--- src/controllers/TipoController.js
+++ src/controllers/TipoController.js
@@ -9,4 +9,17 @@ module.exports = {
 
     return response.json(tipos);
   },
+  async listPorSubTipo(request, response) {
+    const { subTipo } = request.query;
+
+    const tipo = await connection('tipos')
+      .orderBy('tipo')
+      .where('subTipo', '=', subTipo)
+      .select(['tipos.*']).catch(function (err) {
+        console.error(err);
+      });
+
+
+    return response.json(tipo);
+  },
 };
