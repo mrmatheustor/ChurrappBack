08ba3b216d367bb96c0d86b1cd37572c53caa3ea
Rafael França <rafaelfldasilva@gmail.com>
2020-08-15 11:37:06 -0300

    correção para deletar churras convidados e itens do churras

--- src/controllers/ChurrasController.js
+++ src/controllers/ChurrasController.js
@@ -137,6 +137,8 @@ module.exports = {
     }
 
     await connection('churras').where('id', id).delete();
+    await connection('convidados').where('churras_id', id).delete();
+    await connection('listaChurrasco').where('churras_id', id).delete();
 
     return response.status(204).send();
   }
