47cbadf7afb183ec4919f80dc9c0484182cbd969
Rafael França <rafaelfldasilva@gmail.com>
2020-08-15 13:48:56 -0300

    adiciona usuario se ainda nao existe

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -6,106 +6,115 @@ module.exports = {
 
   async list(request, response) {
     const usuarios = await connection('usuarios')
-    .select('*')
-    .catch(function(err) {
-      console.error(err);
+      .select('*')
+      .catch(function (err) {
+        console.error(err);
       });
 
     return response.json(usuarios);
   },
 
   async listCelularUsuario(request, response) {
-    const {celular} = request.params;
+    const { celular } = request.params;
 
     const pessoa = await connection('usuarios')
-    .where('celular', celular)
-    .select('*')
-    .catch(function(err) {
-      console.error(err);
+      .where('celular', celular)
+      .select('*')
+      .catch(function (err) {
+        console.error(err);
       });
-      
+
     return response.json(pessoa);
   },
-  
+
   async listLogado(request, response) {
     const { id } = request.params;
     const { page = 1 } = request.query;
 
     const usuarios = await connection('usuarios')
-    .join('pontoCarne', 'pontoCarne.id', '=', 'usuarios.pontoCarne_id')
-    .join('quantidadeCome', 'quantidadeCome.id', '=', 'usuarios.quantidadeCome_id')
-    .limit(1)
-    .offset((page - 1) * 1)
-    .where('usuarios.id', id)
-    .select(['usuarios.*','pontoCarne.ponto','quantidadeCome.nomeQuantidadeCome'])
-    .catch(function(err) {
-      console.error(err);
+      .join('pontoCarne', 'pontoCarne.id', '=', 'usuarios.pontoCarne_id')
+      .join('quantidadeCome', 'quantidadeCome.id', '=', 'usuarios.quantidadeCome_id')
+      .limit(1)
+      .offset((page - 1) * 1)
+      .where('usuarios.id', id)
+      .select(['usuarios.*', 'pontoCarne.ponto', 'quantidadeCome.nomeQuantidadeCome'])
+      .catch(function (err) {
+        console.error(err);
       });
 
     return response.json(usuarios);
   },
 
   async create(request, response) {
-      const { nome, sobrenome, email, cidade, uf, idade, celular, fotoUrlU, cadastrado, apelido,
-      pontoCarne_id,carnePreferida_id,quantidadeCome_id,bebidaPreferida_id,acompanhamentoPreferido_id } = request.body;
-      var dateTime = require('node-datetime');
-      var dt = dateTime.create();
-      var formatted = dt.format('d/m/Y');
-      const joined = formatted;
+    const { nome, sobrenome, email, cidade, uf, idade, celular, fotoUrlU, cadastrado, apelido,
+      pontoCarne_id, carnePreferida_id, quantidadeCome_id, bebidaPreferida_id, acompanhamentoPreferido_id } = request.body;
+    var dateTime = require('node-datetime');
+    var dt = dateTime.create();
+    var formatted = dt.format('d/m/Y');
+    const joined = formatted;
 
-    const id = crypto.randomBytes(8).toString('HEX');
+    const jaExiste = await connection('usuarios')
+      .where("celular", celular)
+      .select('*')
+      .catch(function (err) {
+        console.error(err.detail);
+      });
 
+      console.log("jaExiste", jaExiste)
+      
+    if (jaExiste == []) {
+      const id = crypto.randomBytes(8).toString('HEX');
 
-    const usuarios = await connection('usuarios').insert({
-      id,
-      nome,
-      sobrenome,
-      email,
-      cidade,
-      uf,
-      idade,
-      joined,
-      fotoUrlU,
-      celular,
-      apelido,
-      pontoCarne_id,
-      carnePreferida_id,
-      quantidadeCome_id,
-      bebidaPreferida_id,
-      acompanhamentoPreferido_id,
-      cadastrado
-    }).catch(function(err) {
-      console.error(err.detail);
-    });
+      const usuarios = await connection('usuarios').insert({
+        id,
+        nome,
+        sobrenome,
+        email,
+        cidade,
+        uf,
+        idade,
+        joined,
+        fotoUrlU,
+        celular,
+        apelido,
+        pontoCarne_id,
+        carnePreferida_id,
+        quantidadeCome_id,
+        bebidaPreferida_id,
+        acompanhamentoPreferido_id,
+        cadastrado
+      }).catch(function (err) {
+        console.error(err.detail);
+      });
 
-    console.log(usuarios);
+      return response.json({
+        id: id,
+        nome: nome,
+        sobrenome: sobrenome,
+        email: email,
+        cidade: cidade,
+        uf: uf,
+        idade: idade,
+        joined: joined,
+        fotoUrlU: fotoUrlU,
+        celular: celular,
+        apelido: apelido,
+        pontoCarne_id: pontoCarne_id,
+        carnePreferida_id: carnePreferida_id,
+        quantidadeCome_id: quantidadeCome_id,
+        bebidaPreferida_id: bebidaPreferida_id,
+        acompanhamentoPreferido_id: acompanhamentoPreferido_id,
+        cadastrado: cadastrado,
+      });
+    }
 
+    return response.json(jaExiste)
 
-    return response.json({
-      id:id,
-      nome:nome,
-      sobrenome:sobrenome,
-      email:email,
-      cidade:cidade,
-      uf:uf,
-      idade:idade,
-      joined:joined,
-      fotoUrlU:fotoUrlU,
-      celular:celular,
-      apelido:apelido,
-      pontoCarne_id:pontoCarne_id,
-      carnePreferida_id:carnePreferida_id,
-      quantidadeCome_id:quantidadeCome_id,
-      bebidaPreferida_id:bebidaPreferida_id,
-      acompanhamentoPreferido_id:acompanhamentoPreferido_id,
-      cadastrado:cadastrado,    
-    });
-    
   },
   async update(request, response) {
-    const {id} = request.params;
-    const { nome, sobrenome, email, cadastrado,cidade, uf, idade, fotoUrlU, celular, apelido,
-      pontoCarne_id,carnePreferida_id,quantidadeCome_id,bebidaPreferida_id,acompanhamentoPreferido_id, joined } = request.body;
+    const { id } = request.params;
+    const { nome, sobrenome, email, cadastrado, cidade, uf, idade, fotoUrlU, celular, apelido,
+      pontoCarne_id, carnePreferida_id, quantidadeCome_id, bebidaPreferida_id, acompanhamentoPreferido_id, joined } = request.body;
 
     const usuarios = await connection('usuarios').where('id', id).update({
       nome,
@@ -124,9 +133,9 @@ module.exports = {
       bebidaPreferida_id,
       acompanhamentoPreferido_id,
       cadastrado
-    }).catch(function(err) {
+    }).catch(function (err) {
       console.error(err);
-      });
+    });
 
     return response.status(200).send()
   }
