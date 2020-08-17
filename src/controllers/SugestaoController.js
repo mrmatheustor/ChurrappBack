11197fe0e437386fa301e949871de9485261e8c1
Rafael França <rafaelfldasilva@gmail.com>
2020-08-16 23:15:32 -0300

    modificações para funcionamento de subtipo em sugestão

--- src/controllers/SugestaoController.js
+++ src/controllers/SugestaoController.js
@@ -4,10 +4,14 @@ const crypto = require('crypto');
 module.exports = {
 
   async list(request, response) {
+    const {subTipo} = request.params;
+
     const sugestao = await connection('sugestao')
     .join('unidades', 'unidades.id', '=', 'sugestao.unidade_id')
     .join('itens', 'itens.id', '=', 'sugestao.item_id')
     .join('tipos', 'tipos.id', '=', 'itens.tipo_id')
+    .join('subTipos', 'tipos.subTipo_id','=','subTipos.id')
+    .where('subTipos.id',subTipo)
     .select(['sugestao.quantidade','unidades.unidade', 'itens.nomeItem', 'tipos.tipo', 'itens.tipo_id'])
     .catch(function(err) {
       console.error(err);
