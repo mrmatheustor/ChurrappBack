f9e1576027422e561c219dc5ab14b9a446dd3d7d
Rafael França <rafaelfldasilva@gmail.com>
2020-08-27 15:39:08 -0300

    adicionadas novas colunas em notificacoes

--- src/database/migrations/20200827141042_create_notificacoes.js
+++ src/database/migrations/20200827141042_create_notificacoes.js
@@ -3,10 +3,12 @@ exports.up = function (knex) {
     return knex.schema.createTable('notificacoes', function (table) {
         table.increments();
         table.string('mensagem').notNullable();
+        table.string('btn1');
+        table.string('btn2');
 
         //foreingKeys
         table.string('usuario_id').notNullable();
-        table.string('churras_id').notNullable();
+        table.string('churras_id');
 
         table.foreign('usuario_id').references('id').inTable('usuarios');
         table.foreign('churras_id').references('id').inTable('churras');
