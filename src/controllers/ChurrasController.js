6b98a3846a0f110ed92a34afc6be2e4ee2b4eabf
Matheus <mrmatheustor@gmail.com>
2020-08-31 08:36:57 -0300

    .

--- src/controllers/ChurrasController.js
+++ src/controllers/ChurrasController.js
@@ -63,7 +63,8 @@ module.exports = {
       .join('convidados', 'convidados.churras_id', '=', 'churras.id')
       .join('usuarios', 'usuarios.id', '=', 'churras.usuario_id')
       .where('convidados.usuario_id', usuario_id)
-      .where('convidados.confirmado', '=', null, 'OR', 'convidados.confirmado', '=', true)
+      .where('convidados.confirmado', '=', null)
+      .orWhere('convidados.confirmado', '=', true)
       .where('data', '<', formatted)
       .orderBy('data')
       .select(['churras.*',
