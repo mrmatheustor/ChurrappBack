3dc1c3979d407970aa2aff59060742efb84335b4
Matheus <mrmatheustor@gmail.com>
2020-09-08 20:39:49 -0300

    .

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -39,6 +39,7 @@ module.exports = {
       .join('itens as bp', 'bp.id', '=', 'usuarios.bebidaPreferida_id')
       .join('itens as sp', 'sp.id', '=', 'usuarios.sobremesaPreferida_id')
       .where('usuarios.id', id)
+      .orderBy('nome')
       .select(['usuarios.*', 'pontoCarne.ponto', 'quantidadeCome.nomeQuantidadeCome', 'cp.nomeItem as carnePreferida', 'sp.nomeItem as sobremesaPreferida', 'ap.nomeItem as acompanhamentoPreferido', 'bp.nomeItem as bebidaPreferida'])
       .catch(function (err) {
         console.error(err);
