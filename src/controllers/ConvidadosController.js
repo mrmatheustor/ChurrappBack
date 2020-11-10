f3a5ad704a334aac318d0d56ed92a36ea5b490a8
Rafael França <rafaelfldasilva@gmail.com>
2020-11-10 11:32:33 -0300

    .

--- src/controllers/ConvidadosController.js
+++ src/controllers/ConvidadosController.js
@@ -163,7 +163,7 @@ module.exports = {
               console.log("Multiplicador "+multiplicador)
               await connection('listaChurrasco')
               .where('churras_id',churras_id)
-              .update('listaChurrasco.quantidade', 'listaChurrasco.quantidade'*multiplicador )
+              .update('quantidade', 'listaChurrasco.quantidade'*multiplicador )
               .catch(function (err) {
                 console.error(err);
               });
