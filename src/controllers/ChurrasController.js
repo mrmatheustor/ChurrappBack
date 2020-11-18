8c0b64954a9aaaa14d8876b835485402ae1a02e0
Rafael França <rafaelfldasilva@gmail.com>
2020-11-17 21:43:23 -0300

    correção churras passados

--- src/controllers/ChurrasController.js
+++ src/controllers/ChurrasController.js
@@ -57,10 +57,10 @@ module.exports = {
   async dataPassado(request, response) {
     const { usuario_id } = request.params;
     var dateTime = require('node-datetime');
-    var dt = dateTime.create(now);
+    var dt = dateTime.create();
     var formatted = dt.format('d/m/Y');
 
-    console.log(dateTime.now())
+    console.log(dt.now())
 
     const churras = await connection('churras')
       .join('convidados', 'convidados.churras_id', '=', 'churras.id')
