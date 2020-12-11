5f4869f7ebec2030cf77803dcc54c8ca59ce08bd
Matheus <mrmatheustor@gmail.com>
2020-12-10 22:56:39 -0300

    .

--- src/controllers/ListaChurrascoController.js
+++ src/controllers/ListaChurrascoController.js
@@ -75,6 +75,7 @@ module.exports = {
     await connection('listaChurrasco')
       .join('churras', 'churras.id', '=', 'listaChurrasco.churras_id')
       .where('listaChurrasco.id', id)
+      .where('listaChurrasco.valorTotal', 'churras.valorTotal')
       .select('listaChurrasco.*', 'churras.id as churras_id', 'churras.valorTotal')
       .update({
         unidade_id: unidade_id,
