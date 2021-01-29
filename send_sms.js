a577161e513e1802dd873b0ba84de27717ebeb1f
Matheus <mrmatheustor@gmail.com>
2021-01-29 16:43:24 -0300

    .

--- send_sms.js
+++ send_sms.js
@@ -1,5 +1,6 @@
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
+const twilioCel = process.env.TWILIO_CEL
 var celular = process.argv[2]
 var pin = process.argv[3]
 
@@ -8,7 +9,7 @@ const client = require('twilio')(accountSid, authToken);
 client.messages
   .create({
      body: 'SEU PIN: ' + pin,
-     from: '+13512009099',
+     from: twilioCel,
      to: '+55021'+celular
    })
   .then(message => console.log(message.sid));
